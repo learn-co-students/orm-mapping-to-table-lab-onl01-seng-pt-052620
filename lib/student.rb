a970a20635f148a1de873b5e60927e1f7a80cc99
M. Gage Morgan <gage@gages.blog>
2020-08-07 17:47:36 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,55 @@
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    # Here lies yet another Flatiron-ism of shoving code in a lesson,
+    # saying they're gonna explain it later, and then never explaining 
+    # what it does or why. Copy pasted out of the lesson because the lab README 
+    # told me to. Tf does last_insert_rowid() do and HOW???
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(attributes)
+    student = self.new(attributes[:name], attributes[:grade])
+    student.save 
+    student 
+  end
 end
